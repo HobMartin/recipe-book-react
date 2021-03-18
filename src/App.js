dff47d6cbd315266149b84f46b3b6ed0d5b4e0a6
HobMartin <ivanderda1999@gmail.com>
2021-03-18 18:21:45 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "05ee4760";
   const APP_KEY = "bed80e643616fa6e8403f45658c2baed";
-  const ExampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(async () => {
+    getRecipies();
+  }, []);
+
+  const getRecipies = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+  };
 
   return (
     <div className="App">
